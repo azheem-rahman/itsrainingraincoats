10a67c1831e4940bf4f7028f8e5eff994abfd391
Azheem Rahman <azheem.rahman@gmail.com>
2023-02-09 22:21:48 +0800

    Revert "fixed create account redirect bug"
    This reverts commit a86d8916733a1ef24729fd5cbbee5235a99fbc83.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -124,16 +124,13 @@ export default function Login(props) {
           </NavLink>
         </form>
       </div>
-
-      {context.isLoggedIn ? <Navigate to="/home" /> : <Navigate to="/login" />}
-
-      {/* {context.isLoggedIn &&
+      {context.isLoggedIn &&
         (context.persona === "donor" || context.hasProfile) && (
           <Navigate to="/home" />
         )}
       {context.isLoggedIn &&
-        context.persona === "worker" &&
-        !context.hasProfile && <Navigate to="/create-account" />} */}
+        !context.hasProfile &&
+        context.persona === "worker" && <Navigate to="/create-account" />}
     </>
   );
 }
